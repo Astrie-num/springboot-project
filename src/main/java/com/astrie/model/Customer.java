507b3048d4306999d1e34a51fa1214759e7fd5e6
Uwumviyimana Asterie <uwasterie07@gmail.com>
2024-05-16 20:34:55 +0200

    Initial commit

---
+++ src/main/java/com/astrie/model/Customer.java
@@ -0,0 +1,77 @@
+package com.astrie.model;
+
+import java.util.Objects;
+
+public class Customer {
+
+        private String name;
+        private int id;
+        private int age;
+        private String  email;
+
+    public Customer(String name, int id, int age, String email) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        this.email = email;
+    }
+
+    public Customer() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return id == customer.id && age == customer.age && Objects.equals(name, customer.name) && Objects.equals(email, customer.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id, age, email);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
