2b49aad5929d87e2c9adce9ddcc191c7fe3c9c1b
Uwumviyimana Asterie <uwasterie07@gmail.com>
2024-05-21 09:40:59 +0200

    Added APIs

--- src/main/java/com/astrie/controllers/CustomerController.java
+++ src/main/java/com/astrie/controllers/CustomerController.java
@@ -4,23 +4,68 @@ package com.astrie.controllers;
 import com.astrie.model.Customer;
 import com.astrie.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8060/customers")
+@RequestMapping("/api/v1/customers")
+@CrossOrigin(origins = "http://localhost:8060")
 
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
 
-    @GetMapping("/api/v1/customers")
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<Customer> getAllCustomers() {
         return customerService.findAll();
 
+    }
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> RegisterCustomer(@RequestBody Customer customer) {
+        try{
+            customerService.addCustomer(customer);
+            return ResponseEntity.ok("Customer added successfully");
+        }
+        catch (RuntimeException e){
+            return ResponseEntity.status(404).body(e.getMessage());
+
+        }
+
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<String> deleteCustomer(@PathVariable int id){
+        try{
+            customerService.deleteCustomerById(id);
+            return ResponseEntity.ok("Customer deleted successfully");
+        }
+        catch (RuntimeException e) {
+            return ResponseEntity.status(404).body(e.getMessage());
+        }
+
+    }
+
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<?> updateCustomer(@PathVariable int id, @RequestBody Customer customer) {
+        try {
+            customer.setId(id);
+            customerService.update(customer);
+            return ResponseEntity.ok("Customer updated successfully");
+        }
+        catch (RuntimeException e) {
+            return ResponseEntity.status(404).body(e.getMessage());
+        }
     }
 }
--- src/main/java/com/astrie/services/CustomerService.java
+++ src/main/java/com/astrie/services/CustomerService.java
@@ -5,7 +5,9 @@ import com.astrie.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -18,5 +20,33 @@ public class CustomerService {
 
     public List<Customer> findAll() {
         return customerRepository.findAll();
+    }
+
+    public void addCustomer(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+//    public void deleteCustomer(Customer customer) {
+//        customerRepository.delete(customer);
+//    }
+
+    public void update(Customer updatedCustomer) {
+        Optional<Customer> optionalCustomer = customerRepository.findById(updatedCustomer.getId());
+
+        if (optionalCustomer.isPresent()) {
+            Customer existingCustomer = optionalCustomer.get();
+            existingCustomer.setName(updatedCustomer.getName());
+            existingCustomer.setEmail(updatedCustomer.getEmail());
+            existingCustomer.setAge(updatedCustomer.getAge());
+
+            customerRepository.save(existingCustomer);
+        }
+        else {
+            throw new RuntimeException("Customer not found with id " + updatedCustomer.getId());
+        }
+    }
+
+    public void deleteCustomerById(int id) {
+        customerRepository.deleteAllById(Collections.singleton(id));
     }
 }
